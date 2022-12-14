4a4506ab73e32004f9c86badd2f625668a3a2879
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2022-12-14 22:53:54 +0100

    add variable name continuity

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -5,20 +5,20 @@ import { Product } from "../entity/Product";
 export class ProductController {
   private productRepository = AppDataSource.getRepository(Product);
 
-  async add(req: Request, res: Response, next: NextFunction) {
+  async add(req: Request, _res: Response) {
     return this.productRepository.save(req.body);
   }
 
-  async remove(req: Request, res: Response, next: NextFunction) {
+  async remove(req: Request, _res: Response) {
     var product = await this.productRepository.findOneBy({ id: req.params.id });
     await this.productRepository.remove(product);
   }
 
-  async getAll(req: Request, res: Response, next: NextFunction) {
+  async getAll(req: Request, _res: Response) {
     return this.productRepository.find();
   }
 
-  async getOne(req: Request, res: Response, next: NextFunction) {
+  async getOne(req: Request, _res: Response) {
     return this.productRepository.findOne({ where: { id: req.params.id } });
   }
 }
--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -5,21 +5,21 @@ import { User } from "../entity/User";
 export class UserController {
   private userRepository = AppDataSource.getRepository(User);
 
-  async all(request: Request, response: Response, next: NextFunction) {
+  async all(req: Request, _res: Response) {
     return this.userRepository.find();
   }
 
-  async getOne(request: Request, response: Response, next: NextFunction) {
-    return this.userRepository.findOne({ where: { id: request.params.id } });
+  async getOne(req: Request, _res: Response) {
+    return this.userRepository.findOne({ where: { id: req.params.id } });
   }
 
-  async add(request: Request, response: Response, next: NextFunction) {
-    return this.userRepository.save(request.body);
+  async add(req: Request, _res: Response) {
+    return this.userRepository.save(req.body);
   }
 
-  async remove(request: Request, response: Response, next: NextFunction) {
+  async remove(req: Request, _res: Response) {
     let userToRemove = await this.userRepository.findOneBy({
-      id: request.params.id,
+      id: req.params.id,
     });
     await this.userRepository.remove(userToRemove);
   }
